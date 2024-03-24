21cb6ec430f9abf1c9092c3f4d4cc2d215706b3d
Royhan <royhankeren12@gmail.com>
2024-03-24 16:18:47 +0700

    add AdminUserSeeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -14,7 +14,7 @@ class AdminUserSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('admin_users')-> inserts([
+        DB::table('admin_users') -> insert([
             "name" => "Sha Admin",
             "email" => "admin@bwabank.com",
             "password" => bcrypt("rahasia123")
