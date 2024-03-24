de6de98b26265d822384452d6dad59eac70dbadf
Royhan <royhankeren12@gmail.com>
2024-03-24 16:18:13 +0700

    add AdminUserSeeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -2,18 +2,20 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
+use Illuminate\Database\Seeder; 
 use Illuminate\Support\Facades\DB;
 
 class AdminUserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     * 
+     * @return void
      */
     public function run(): void
     {
-        DB::table("admin_users")->insert([
-            "name" => "sha Admin",
+        DB::table('admin_users')-> inserts([
+            "name" => "Sha Admin",
             "email" => "admin@bwabank.com",
             "password" => bcrypt("rahasia123")
         ]);
